042d4816a5ec82f2b473f81357c415c7fc50d78a
Vitalii Shapovalov <martyit@ukr.net>
2024-04-22 21:00:40 +0200

    added const

---
+++ script.js
@@ -0,0 +1,165 @@
+
+const testSet = [
+	{
+		question: 'What is the capital of France?',
+		options: [
+			{
+				id: 1,
+				text: 'Paris',
+			},
+			{
+				id: 2,
+				text: 'London',
+			},
+			{
+				id: 3,
+				text: 'Berlin',
+			},
+		],
+		correctAnswerId: 1,
+	},
+	{
+		question: 'What is the largest planet in the Solar System?',
+		options: [
+			{
+				id: 1,
+				text: 'Earth',
+			},
+			{
+				id: 2,
+				text: 'Mars',
+			},
+			{
+				id: 3,
+				text: 'Jupiter',
+			},
+		],
+		correctAnswerId: 3,
+	},
+	{
+		question: 'What animal is the symbol of the Olympic Games?',
+		options: [
+			{
+				id: 1,
+				text: 'Owl',
+			},
+			{
+				id: 2,
+				text: 'Bear',
+			},
+			{
+				id: 3,
+				text: 'Lion',
+			},
+		],
+		correctAnswerId: 3,
+	},
+	{
+		question: 'Which country is famous for its cheesy dish lasagna?',
+		options: [
+			{
+				id: 1,
+				text: 'Italy',
+			},
+			{
+				id: 2,
+				text: 'Spain',
+			},
+			{
+				id: 3,
+				text: 'Greece',
+			},
+		],
+		correctAnswerId: 1,
+	},
+	{
+		question: 'What number is considered lucky in many cultures?',
+		options: [
+			{
+				id: 1,
+				text: '13',
+			},
+			{
+				id: 2,
+				text: '7',
+			},
+			{
+				id: 3,
+				text: '3',
+			},
+		],
+		correctAnswerId: 2,
+	},
+	{
+		question: 'Which language is the most spoken in the world by number of native speakers?',
+		options: [
+			{
+				id: 1,
+				text: 'English',
+			},
+			{
+				id: 2,
+				text: 'Spanish',
+			},
+			{
+				id: 3,
+				text: 'Chinese',
+			},
+		],
+		correctAnswerId: 3,
+	},
+	{
+		question: 'Which river is the longest on Earth?',
+		options: [
+			{
+				id: 1,
+				text: 'Nile',
+			},
+			{
+				id: 2,
+				text: 'Amazon',
+			},
+			{
+				id: 3,
+				text: 'Mississippi',
+			},
+		],
+		correctAnswerId: 1,
+	},
+	{
+		question: 'Which mountain is considered the highest on Earth?',
+		options: [
+			{
+				id: 1,
+				text: 'Elbrus',
+			},
+			{
+				id: 2,
+				text: 'Alps',
+			},
+			{
+				id: 3,
+				text: 'Himalayas',
+			},
+		],
+		correctAnswerId: 3,
+	},
+	{
+		question: 'Which plant is the symbol of Ireland?',
+		options: [
+			{
+				id: 1,
+				text: 'Truffle',
+			},
+			{
+				id: 2,
+				text: 'Red rose',
+			},
+			{
+				id: 3,
+				text: 'Shamrock',
+			},
+		],
+		correctAnswerId: 2,
+	},
+]
